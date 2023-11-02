8b4ab50d9b93b584fe94fd495c4e16a600b60ba4
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2023-11-02 14:05:05 +0800

    mmm

---
+++ SBIT3J_SuperSystem/SBIT3J_SuperSystem/Controllers/InventoryManagementController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBIT3J_SuperSystem.Controllers
+{
+    public class InventoryManagementController : Controller
+    {
+        // GET: InventoryManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+        public ActionResult InventoryArchives()
+        {
+            return View();
+        }
+        public ActionResult Restock()
+        {
+            return View();
+        }
+        public ActionResult Returns()
+        {
+            return View();
+        }
+    }
+}
