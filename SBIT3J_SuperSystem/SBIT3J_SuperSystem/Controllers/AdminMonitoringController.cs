b2fcf3b13ca7fba85a8ad30a0cfd81f76e6783d0
Makoo <141389899+makuu12@users.noreply.github.com>
2023-11-21 18:58:35 +0800

    config changes

--- SBIT3J_SuperSystem/SBIT3J_SuperSystem/Controllers/AdminMonitoringController.cs
+++ SBIT3J_SuperSystem/SBIT3J_SuperSystem/Controllers/AdminMonitoringController.cs
@@ -9,12 +9,43 @@ namespace SBIT3J_SuperSystem.Controllers
     public class AdminMonitoringController : Controller
     {
         // GET: AdminMonitoring
-        public ActionResult Index()
+        public ActionResult Index() // Revenue Dashboard
+        {
+            return View(); // View the overall Summary Income of the company
+            //ISSUE: finished interface but need a dynamic data
+        }
+        public ActionResult SalesRevenue()
+        {
+            return View(); // The sale performance of the specified date
+            //ISSUE: have an API chart (line 113)
+        }
+        public ActionResult ProductRevenue()
+        {
+            return View(); // The product performance of the specified date
+            //ISSUE:
+        }
+        public ActionResult CompanyProfit()
+        {
+            return View();
+            //ISSUE: finished interface but need a dynamic data
+        }
+        public ActionResult LossesDashboard()
+        {
+            return View();
+            //ISSUE: The th side cannot center the text
+        }
+
+        public ActionResult CashierActivities()
         {
             return View();
         }
 
-        public ActionResult SalesRevenue()
+        public ActionResult OverallActivities()
+        {
+            return View();
+        }
+
+        public ActionResult Inventory()
         {
             return View();
         }
